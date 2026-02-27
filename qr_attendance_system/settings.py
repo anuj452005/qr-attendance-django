de37fda703d86744c0c21fb6e4526004ec16f06e
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2026-02-27 13:55:28 +0530

    Fix STATIC_URL for Render deployment

--- qr_attendance_system/settings.py
+++ qr_attendance_system/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = env('STATIC_URL', default='static/')
+STATIC_URL = env('STATIC_URL', default='/static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
